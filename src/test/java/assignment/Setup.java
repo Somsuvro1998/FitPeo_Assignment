e70ba65765c76b5d2849200475383aa6ab6bf3f1
Somsuvro1998 <Palz.somsuvro@Gmail.com>
2024-08-04 21:17:19 +0530

    1st push

--- src/test/java/assignment/Setup.java
+++ src/test/java/assignment/Setup.java
@@ -10,8 +10,8 @@
  * Click on Next button below.
  * Select Clone URI.
  * Again click on Next button below
- * Provide the Github Repositary URL in the URI TextField.
- * The Host and Repositary path should be visible.
+ * Provide the Github Repository URL in the URI TextField.
+ * The Host and Repository path should be visible.
  * Then click on Next Button below.
  * wait for the project to load and then click on Next Button again.
  * In the Local Destination page either choose the directory where you want to save the project 
@@ -23,7 +23,27 @@
  * Double click on src/test/java to expand.
  * Double click on assignment package to expand.
  * Open FitPeo.java, right click, go to Run As and click on 1 TestNg Test.
- *  
+ * The test script will start executing.
  * 
+ * 
+ * If using GitBash:-
+ * Go the folder in your local system where you want to store the project.
+ * Right click and select Open Git Bash here or click on show more options and then click on Open Git Bash here.
+ * in the Git Bash window provide command git clone <provide the full repository url> and press enter.
+ * The project will be cloned from Github to your local system.
+ * Open Eclipse IDE and click on File on the top left corner.
+ * Click on import.
+ * Double click on General to expand.
+ * Click on Projects from Folder or Archive and then Click on Next button.
+ * Click on Directory and select the project folder (in this case it will be FitPeo_Assignment) and click on Finish.
+ * Now the Project will be visible in your Project Explorer section on the left side.
+ * Double click on the project to expand.
+ * Double click on src/test/java to expand.
+ * Double click on assignment package to expand.
+ * Open FitPeo.java, right click, go to Run As and click on 1 TestNg Test.
+ * The test script will start executing.
+ * 
+ * 
+ *
  */
    
